a6f45e75da9c9944646851a35ebea386af0ba853
Amir <mfathulqorib97@gmail.com>
2025-03-09 20:46:30 +0700

    change title

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from libs import Library, Member
 
-st.title("Library Membership System")
+st.title("Library Membership System Automation")
 
 if "library" not in st.session_state:
     st.session_state.library = Library("My Library")
